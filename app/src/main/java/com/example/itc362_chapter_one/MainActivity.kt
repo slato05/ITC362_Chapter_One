bacb30b02ebbe2b07859743133f00df12e0ea71d
slato05 <slato2001@gmail.com>
2024-08-30 15:40:08 -0500

    Working Version

--- app/src/main/java/com/example/itc362_chapter_one/MainActivity.kt
+++ app/src/main/java/com/example/itc362_chapter_one/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.example.itc362_chapter_one
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+
+
+        }
     }
 }
